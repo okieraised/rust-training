68e465b8cfebc465183a5a5bf55010355cdd05b7
tripg <phamtri2895@gmail.com>
2023-04-26 11:00:06 +0700

    Exercise 3 (cont)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -17,4 +17,34 @@ fn main() {
         println!("inner x is {x}");
     }
     println!("outer x is {x}");
+
+    let spaces = "    ";
+
+    let spaces = spaces.len();
+
+    println!("len spaces is {spaces}");
+
+    let sum = 5 + 10;
+    println!("sum is {sum}");
+
+    let difference = 95.5 - 4.3;
+    println!("difference is {difference}");
+
+    let product = 4 * 30;
+    println!("product is {product}");
+
+    let quotient = 56.7 / 32.2;
+    println!("quotient is {quotient}");
+
+    let truncated = -5 / 3;
+    println!("truncated is {truncated}");
+
+    let remainder = 43 % 5;
+    println!("remainder is {remainder}");
+
+    let c = 'z';
+    println!("c is {c}");
+
+    let heart_eyed_cat = '😻';
+    println!("heart eyed cat is {heart_eyed_cat}");
 }

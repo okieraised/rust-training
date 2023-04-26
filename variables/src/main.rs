129f8b0bdb40985a6099b1d13cfffd923a48ba7a
tripg <phamtri2895@gmail.com>
2023-04-26 10:03:19 +0700

    exercise 3 (cont)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("x is {x}");
+
+    x = 6;
+    println!("x is {x}");
+
+    const THREE_HOUR_IN_SECONDS: u32 = 60 * 60 * 3;
+
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("inner x is {x}");
+    }
+    println!("outer x is {x}");
 }

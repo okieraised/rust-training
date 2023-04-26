01c66c07e0ac3c90eea75f88fb13e348bd154cd9
tripg <phamtri2895@gmail.com>
2023-04-26 11:21:22 +0700

    Exercise 3 (cont)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 5;
     println!("x is {x}");
@@ -24,6 +26,7 @@ fn main() {
 
     println!("len spaces is {spaces}");
 
+    // Numeric
     let sum = 5 + 10;
     println!("sum is {sum}");
 
@@ -42,9 +45,46 @@ fn main() {
     let remainder = 43 % 5;
     println!("remainder is {remainder}");
 
+    // Char type
     let c = 'z';
     println!("c is {c}");
 
     let heart_eyed_cat = '😻';
     println!("heart eyed cat is {heart_eyed_cat}");
+
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("tuple: x is {x}, y is {y}, z is {z}");
+
+    let first = tup.0;
+
+    let second = tup.1;
+
+    let third = tup.2;
+
+    println!("first is {first}, second is {second}, third is {third}");
+
+    // Array
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index).expect("failed to read line");
+
+    let index: usize = index.trim().parse().expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("value at index {index}: {element}");
 }

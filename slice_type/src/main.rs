d7d8bb8dc2153c642caf53fc4cd8b7151970d73e
okieraised <phamtri2895@gmail.com>
2023-05-22 20:54:08 +0700

    slice_type exercise 1

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello, world!");
+
+    let word = first_word(&s);
+
+    println!("first word is '{}'", word);
+
+    s.clear();
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }

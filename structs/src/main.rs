8891aee99f648820ba82f3c6fc8e0d026836b149
okieraised <phamtri2895@gmail.com>
2023-05-22 21:50:42 +0700

    struct exercises 1

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let user1  = User {
+        active: true,
+        username: String::from("tripg"),
+        email: String::from("tripg@mail.com"),
+        sign_in_count: 1,
+    };
+
+    println!("{}", user1.sign_in_count);
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }

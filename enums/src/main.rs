996c19be972cd639e353902d4e48a15528d57b69
okieraised <phamtri2895@gmail.com>
2023-05-25 14:53:13 +0700

    fix enum error

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -16,9 +16,9 @@ fn main() {
     route(four);
     route(six);
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
 
 }

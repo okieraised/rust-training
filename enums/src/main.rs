432453c4ab8e18ce0a03e675ba1cadceeecc111a
okieraised <phamtri2895@gmail.com>
2023-05-25 15:21:18 +0700

    enum exercise 2

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -35,6 +35,13 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
 
     let four = IpAddrKind::V4(String::from("127.0.0.1"));
@@ -59,8 +66,27 @@ fn main() {
     // let sum = x + y;
 
     let val = value_in_cents(Coin::Quarter(UsState::Oklahoma));
-    println!("coin value is {}", val)
-    
+    println!("coin value is {}", val);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
 }
 
 fn route(ip_kind: IpAddrKind) {}
+
+fn add_fancy_hat() {
+    println!("haha")
+}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {
+    println!("{}", num_spaces)
+}

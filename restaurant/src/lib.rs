0de16413cd44a2252b13139029f2933a4f7be0b2
tripg <phamtri2895@gmail.com>
2023-05-30 14:04:05 +0700

    module exercise 2

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -13,6 +13,17 @@ mod front_of_house {
         fn take_payment() {}
 
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_wrong_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {

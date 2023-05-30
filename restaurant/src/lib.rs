fa1a7e2edbd8f542f002d1923d3b0154fdec12de
tripg <phamtri2895@gmail.com>
2023-05-30 12:02:31 +0700

    lib exercises

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,4 +1,4 @@
-mod font_of_house {
+mod front_of_house {
     mod hosting {
         fn add_to_waitlist() {}
 
@@ -13,6 +13,12 @@ mod font_of_house {
         fn take_payment() {}
 
     }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
 }
 
 

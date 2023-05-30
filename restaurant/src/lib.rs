a189c31a6044710b5b3dd8cd8584508df3f6be87
tripg <phamtri2895@gmail.com>
2023-05-30 14:15:12 +0700

    module exercise 4

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -19,6 +19,11 @@ fn deliver_order() {}
 
 mod back_of_house {
 
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -46,6 +51,10 @@ pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
 
 
